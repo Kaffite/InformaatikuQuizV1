1b1fe83a08ac8b666d0baf858c51a76c707097b2
Kaffite <karlmarkushannust@gmail.com>
2025-06-18 22:36:50 +0300

    algus

---
+++ src/Questions.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Questions {
+    // Loeb failist küsimused sisse? siin v main meetodis?
+    // Hashmap
+    // Meetod, kus ta küsib random küsimuse
+
+    private Map questionMap;
+
+    public Questions(Map questionMap){
+        this.questionMap = questionMap;
+    }
+
+}
+
